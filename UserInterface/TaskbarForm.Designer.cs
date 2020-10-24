a45d0d80173c2e5a7f6fe929543e0f1c3bccf187
Peter Kirmeier <topeterk@users.noreply.github.com>
2020-10-24 19:50:15 +0200

    [BUG] Corrected DPI Awareness Mode
    Pre-set Per-Monitor DPI Awareness Mode via new Manifest file
    This also resolves #137 as window will be drawn at correct location

--- UserInterface/TaskbarForm.Designer.cs
+++ UserInterface/TaskbarForm.Designer.cs
@@ -33,8 +33,8 @@
             // 
             // TaskbarForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(120, 0);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
