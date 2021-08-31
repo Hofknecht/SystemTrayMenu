2c16ade92f056feb3de8b80c774bf4149b557d6f
Markus Hofknecht <Markus@Hofknecht.eu>
2021-08-31 20:01:02 +0200

    Revert "Revert [Feature] Update to .NET 5 (#157), version 1.0.18.0"

--- UserInterface/Menu.cs
+++ UserInterface/Menu.cs
@@ -11,6 +11,7 @@ namespace SystemTrayMenu.UserInterface
     using System.IO;
     using System.Reflection;
     using System.Windows.Forms;
+    using Svg;
     using SystemTrayMenu.DataClasses;
     using SystemTrayMenu.DllImports;
     using SystemTrayMenu.Helpers;
