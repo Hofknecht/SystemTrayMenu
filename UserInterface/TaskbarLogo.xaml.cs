fb18265395a1d0f11b8517d1e24d2aa8060d6907
Peter Kirmeier <topeterk@users.noreply.github.com>
2023-09-22 20:39:43 +0200

    Fix missed activation events from taskbar window

--- UserInterface/TaskbarLogo.xaml.cs
+++ UserInterface/TaskbarLogo.xaml.cs
@@ -24,14 +24,25 @@ namespace SystemTrayMenu.UserInterface
             Title = myname;
 
             Closed += (_, _) => Application.Current.Shutdown();
-            ContentRendered += MoveOutOfScreen;
+
+            // Do final initialization after rendering has been finished for the first time.
+            // This ensures all icons and images are properly loaded and renderd (e.g. thumbnail image of alt tab menu).
+            ContentRendered += LateInitialize;
         }
 
-        private void MoveOutOfScreen(object? sender, EventArgs e)
+        private void LateInitialize(object? sender, EventArgs e)
         {
             // Do this only once
-            ContentRendered -= MoveOutOfScreen;
+            ContentRendered -= LateInitialize;
+
+            // Move the window out of screen, just for safety
             Top += SystemParameters.VirtualScreenHeight;
+
+            // There is nothing to see, so no need to show this window.
+            // Therefore it shall always be in minimized state.
+            // Further, we then can rely on every activating event.
+            WindowState = WindowState.Minimized;
+            StateChanged += (_, _) => WindowState = WindowState.Minimized;
         }
     }
 }
